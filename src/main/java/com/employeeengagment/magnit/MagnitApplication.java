3df083037bf5d989fa2493f191c5a08e72f0c74c
kgovardhan409 <kgovardhan409@gmail.com>
2023-01-26 23:55:08 +0530

    first commit

---
+++ src/main/java/com/employeeengagment/magnit/MagnitApplication.java
@@ -0,0 +1,13 @@
+package com.employeeengagment.magnit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MagnitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MagnitApplication.class, args);
+	}
+
+}
